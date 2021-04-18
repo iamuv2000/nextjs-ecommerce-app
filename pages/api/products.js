482bb54be024f80d3f7d1afd28868f726fc7dcf0
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2021-04-18 21:10:52 +0530

    Integrated create product route

--- pages/api/products.js
+++ pages/api/products.js
@@ -3,9 +3,45 @@ import Product from '../../models/Product';
 
 initDB();
 
-export default (req, res) => {
+export default async (req, res) => {
+	
+	switch(req.method) {
+		case 'GET' :
+			await getAllProducts(req,res);
+			break;
+		case 'POST':
+			await saveProduct(req,res);
+			break;
+
+	}
+}
+
+const getAllProducts = async ( req, res ) => {
 	console.log("Request for products received")
 	Product.find().then((products) => {
 		res.status(200).send(products)
 	})
 }
+
+const saveProduct = async (req,res) => {
+	try{
+		console.log("Creating product...")
+		const {name,price,description,mediaUrl} = req.body;
+		if(!name || !price || !description || !mediaUrl){
+			res.status(422).json({
+				error: "Please fill all the fields"
+			})
+			return;
+		}
+		const product = await new Product({name,price,description,mediaUrl}).save();
+		res.status(201).json(product)
+	} catch (e) {
+		console.log(e.message)
+		res.status(500).json({
+			error: e.message
+		})
+		return;
+	}
+	
+}
+
--- pages/create.js
+++ pages/create.js
@@ -1,10 +1,48 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import {useState} from 'react';
+import baseURL from '../helpers/baseURL';
 
 const CreatePage = () => {
+
+	const [name , setName] = useState('');
+	const [price , setPrice] = useState('');
+	const [media , setMedia] = useState('');
+	const [description , setDescription] = useState('');
+	
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const res = await fetch(`${baseURL}/api/products` , {
+			'method': 'POST',
+			'headers' : {
+				'Content-Type' : 'application/json'
+			},
+			body: JSON.stringify({
+				name,
+				price,
+				mediaUrl: media,
+				description
+			})
+		})
+		const data = await res.json();
+		if(data.error) {
+			M.toast({html: data.error, classes: 'red'})
+		} else {
+			M.toast({html: 'Product created', classes: 'green'})
+		}
+	}
+
 	return(
-		<div>
-			<h1>Create page</h1>
-		</div>
+		<form className='container' onSubmit={(e)=>handleSubmit(e)}>
+			<input type='text' name='name' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+			<input type='text' name='price' placeholder='Price' value={price} onChange={(e) => setPrice(e.target.value)}/>
+			<input type='text' name='mediaURL' placeholder='Image URL' value={media} onChange={(e) => setMedia(e.target.value)}/>
+			<img className='responsive' src={media} height='400px'/>
+			<textarea name='description' placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} className="materialize-textarea"></textarea>
+			<button className="btn waves-effect waves-light" type="submit">Submit
+   				<i className="material-icons right">send</i>
+  			</button>
+        
+		</form>
 	)
 }
 
